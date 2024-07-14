26fe6dec68f386cad4003ca7f9748f7efe680e48
Eda Bahar <edaabahar@gmail.com>
2024-07-14 14:09:38 +0300

    Create KitchenObjectFeatures.cs

---
+++ Models/KitchenObjects/KitchenObjectFeatures.cs
@@ -0,0 +1,13 @@
+
+
+class KitchenObjectFeatures
+{
+    string material = string.Empty;
+    List<float> position;
+
+    float durability;
+    string texture = string.Empty;
+    float age;
+    float volume;
+    float temperature;
+}
