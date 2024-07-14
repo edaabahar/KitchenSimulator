ab70f16f8ac70c8c9199f017d16f01fd1c9d5ab3
Eda Bahar <edaabahar@gmail.com>
2024-07-14 14:09:35 +0300

    Update KitchenObject.cs

--- Models/KitchenObject.cs
+++ Models/KitchenObject.cs
@@ -2,17 +2,19 @@ using System.Collections.Generic;
 
 class KitchenObject : IKitchenObject
 {
-    string material = string.Empty;
-    List<float> position;
 
-    float durability;
-    string texture = string.Empty;
-    float age;
-    float volume;
-    float temperature;
+    KitchenObjectFeatures Features { get; set; } // TODO get all from this class
+    // string material = string.Empty;
+    // List<float> position;
+
+    // float durability;
+    // string texture = string.Empty;
+    // float age;
+    // float volume;
+    // float temperature;
 
     public string GetName()
     {
-        return this.GetType().ToString();
+        return GetType().ToString();
     }
 }
